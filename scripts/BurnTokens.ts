ad9532a921c03aaa9986376dcf4662e2f3995d9c
Tim Liner <me@timliner.com>
2024-09-23 22:38:46 -0600

    create BridgeableToken contract and related scripts

---
+++ scripts/BurnTokens.ts
@@ -0,0 +1,78 @@
+// npx ts-node --files ./scripts/BurnTokens.ts CONTRACT_ADDRESS AMOUNT NETWORK
+
+import { createPublicClient, http, createWalletClient } from "viem";
+
+import { privateKeyToAccount } from "viem/accounts";
+import { baseSepolia, sepolia } from "viem/chains";
+import { abi } from "../artifacts/contracts/BridgeableToken.sol/BridgeableToken.json";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const providerApiKey = process.env.ALCHEMY_API_KEY || "";
+const minterPrivateKey = process.env.PRIVATE_KEY || "";
+
+function validateParameters(parameters: string[]) {
+  if (!parameters || parameters.length < 3)
+    throw new Error("Parameters not provided");
+
+  const contractAddress = parameters[0] as `0x${string}`;
+  if (!contractAddress) throw new Error("Contract address not provided");
+  if (!/^0x[a-fA-F0-9]{40}$/.test(contractAddress))
+    throw new Error("Invalid contract address");
+
+  const amount = parameters[1];
+  if (isNaN(Number(amount))) throw new Error("Invalid amount");
+
+  const network = parameters[2];
+  if (!network) throw new Error("Network not provided");
+
+  return { contractAddress, amount, network };
+}
+
+async function main() {
+  console.log("\n");
+  const { contractAddress, amount, network } = validateParameters(process.argv.slice(2));
+
+  const chain = network === "base" ? baseSepolia : sepolia;
+  const subdomain = network === "base" ? "base-sepolia" : "eth-sepolia";
+  const transport = http(`https://${subdomain}.g.alchemy.com/v2/${providerApiKey}`);
+
+  const account = privateKeyToAccount(`0x${minterPrivateKey}`);
+  const minter = createWalletClient({
+    account,
+    chain: chain,
+    transport: transport,
+  });
+
+  console.log(`Burning ${amount} tokens from account: ${account.address}`);
+  console.log("Confirm? (Y/n)");
+
+  const stdin = process.stdin;
+  stdin.on("data", async function (d) {
+    if (d.toString().trim() == "Y") {
+      const hash = await minter.writeContract({
+        address: contractAddress,
+        abi,
+        functionName: "burn",
+        args: [BigInt(amount)],
+      });
+      console.log("Transaction hash:", hash);
+      console.log("Waiting for confirmations...");
+      const publicClient = createPublicClient({
+        chain: chain,
+        transport: transport,
+      });
+      const receipt = await publicClient.waitForTransactionReceipt({ hash });
+      console.log(`Transaction confirmed: ${receipt.status}`);
+      console.log(`Block: ${receipt.blockNumber}`)
+    } else {
+      console.log("Operation cancelled");
+    }
+    process.exit();
+  });
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
---
+++ scripts/TokenStatus.ts
@@ -0,0 +1,62 @@
+// npx ts-node --files ./scripts/TokenStatus.ts CONTRACT_ADDRESS ACCOUNT_ADDRESS NETWORK
+
+import { createPublicClient, http } from "viem";
+
+import { baseSepolia, sepolia } from "viem/chains";
+import { abi } from "../artifacts/contracts/BridgeableToken.sol/BridgeableToken.json";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const providerApiKey = process.env.ALCHEMY_API_KEY || "";
+
+function validateParameters(parameters: string[]) {
+  if (!parameters || parameters.length < 3)
+    throw new Error("Parameters not provided");
+
+  const contractAddress = parameters[0] as `0x${string}`;
+  if (!contractAddress) throw new Error("Contract address not provided");
+  if (!/^0x[a-fA-F0-9]{40}$/.test(contractAddress))
+    throw new Error("Invalid contract address");
+
+  const accountAddress = parameters[1] as `0x${string}`;
+  if (!accountAddress) throw new Error("Account address not provided");
+  if (!/^0x[a-fA-F0-9]{40}$/.test(accountAddress))
+    throw new Error("Invalid account address");
+
+  const network = parameters[2];
+  if (!network) throw new Error("Network not provided");
+
+  return { contractAddress, accountAddress, network };
+}
+
+async function main() {
+  console.log("\n");
+  const { contractAddress, accountAddress, network } = validateParameters(process.argv.slice(2));
+
+  const chain = network === "base" ? baseSepolia : sepolia;
+  const subdomain = network === "base" ? "base-sepolia" : "eth-sepolia";
+  const transport = http(`https://${subdomain}.g.alchemy.com/v2/${providerApiKey}`);
+
+  const publicClient = createPublicClient({
+    chain: chain,
+    transport: transport,
+  });
+  const balance = (await publicClient.readContract({
+    address: contractAddress,
+    abi,
+    functionName: "balanceOf",
+    args: [accountAddress],
+  })) as BigInt;
+
+  console.log(
+    `Account: ${accountAddress}\n`,
+    `Tokens: ${balance.toString()}`
+  );
+
+  process.exit();
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
