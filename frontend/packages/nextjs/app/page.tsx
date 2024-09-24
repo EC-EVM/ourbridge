906dd926996478638fba668574730043311f6e40
Tim Liner <me@timliner.com>
2024-09-24 11:18:34 -0600

    display token balances in frontend

---
+++ frontend/packages/nextjs/app/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import Link from "next/link";
+import type { NextPage } from "next";
+import { useAccount } from "wagmi";
+import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { Address } from "~~/components/scaffold-eth";
+import { TokenBalances } from "~~/components/TokenBalances";
+
+const Home: NextPage = () => {
+  const { address: connectedAddress } = useAccount();
+
+  return (
+    <>
+      <div className="flex items-center flex-col flex-grow pt-10">
+        <div className="px-5">
+          <h1 className="text-center">
+            <span className="block text-2xl mb-2">Welcome to</span>
+            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
+          </h1>
+          <div className="flex justify-center items-center space-x-2 flex-col sm:flex-row">
+            <p className="my-2 font-medium">Connected Address:</p>
+            <Address address={connectedAddress} />
+          </div>
+          <TokenBalances></TokenBalances>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
